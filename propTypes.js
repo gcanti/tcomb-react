f75c5ec872d230d65e4d9cc2ea0db585cb865d23
gcanti <giulio.canti@gmail.com>
2014-11-17 14:54:49 +0100

    updated `toPropTypes`

--- propTypes.js
+++ propTypes.js
@@ -3,22 +3,20 @@
 var t = require('tcomb-validation');
 
 function toPropTypes(Struct) {
-  
   var propTypes = {};
   var props = Struct.meta.props;
-  
   Object.keys(props).forEach(function (k) {
     // React custom prop validator
     // see http://facebook.github.io/react/docs/reusable-components.html
-    propTypes[k] = function (values, name, component) {
-      var opts = {
-        path: ['this.props.' + name], 
-        messages: ':path of value `:actual` supplied to `' + component + '`, expected a `:expected`'
-      };
-      return t.validate(values[name], props[name], opts).firstError();
+    propTypes[k] = function getPropType(values, name, component) {
+      var type = props[name];
+      var value = values[name];
+      var err = t.validate(value, type).firstError();
+      if (err) {
+        return new Error(t.util.format('%s is `%s`, should be a `%s`', name, value, t.util.getName(type)));
+      }
     }
   });
-
   return propTypes;
 }
 
